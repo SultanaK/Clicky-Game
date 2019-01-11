5e04fc491786cbb97f1adedf56571960242759a6
Sultana <kr.shampa@yahoo.com>
2019-01-10 18:41:35 -0600

    Updates

---
+++ precache-manifest.b544906c02f8910d67471e5d4687c190.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "9035aa2de130083e30b2",
+    "url": "/Clicky-Game/static/js/main.9035aa2d.chunk.js"
+  },
+  {
+    "revision": "0402d545da934a5c9138",
+    "url": "/Clicky-Game/static/js/1.0402d545.chunk.js"
+  },
+  {
+    "revision": "9035aa2de130083e30b2",
+    "url": "/Clicky-Game/static/css/main.71509f33.chunk.css"
+  },
+  {
+    "revision": "9296467e5b257554cebca394ff18d9b7",
+    "url": "/Clicky-Game/index.html"
+  }
+];
